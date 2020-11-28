44095bfd20ca14f76067bcab588f931553cd4264
u73535508 <emrekaraduman@std.iyte.edu.tr>
2020-11-28 20:46:01 +0000

    add

---
+++ lab6/example1.py
@@ -0,0 +1,15 @@
+mail = 'ceng113@example.com'
+
+email = input('Enter your e-mail\n').lower()
+parts_of_mail = email.split('@')
+
+parts_of_mail[0] = parts_of_mail[0].replace('.','')
+
+last_mail = parts_of_mail[0]+'@'+parts_of_mail[1]
+
+
+if last_mail==mail:
+    print('true')
+else:
+    print('false')
+
