247ffe94bd7ac2918a733c08450b0815a79c0c90
u73535508 <emrekaraduman@std.iyte.edu.tr>
2020-12-05 13:58:59 +0000

    ad

--- lab7/example4.py
+++ lab7/example4.py
@@ -1,15 +1,15 @@
 employees ={}
-salaryy =[]
-mak = []
+whole_salaries =[]
+max_salary = []
 for i in range(5):
     name = input("Name:")
     salary = input("Salary:")
     employees[name] = salary
 for i in employees.values():
-    salaryy.append(i)
+    whole_salaries.append(i)
 for i in range(3):
-    mak.append(max(salaryy))
-    salaryy.remove(max(salaryy))
+    max_salary.append(max(whole_salaries))
+    whole_salaries.remove(max(whole_salaries))
 for a,b in employees.items():
-    if b in mak:
+    if b in max_salary:
         print(a)
